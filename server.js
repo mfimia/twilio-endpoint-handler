0e25a2a67c317155a9bfa3dcb984a722913318bd
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-02-17 13:49:40 +0100

    set up bodyparser and voice + message routes

--- server.js
+++ server.js
@@ -1,44 +1,19 @@
-const twilio = require("twilio");
 const express = require("express");
-const VoiceResponse = require("twilio").twiml.VoiceResponse;
-const ngrok = require("ngrok");
 const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-// expose localhost
-(async function () {
-  const url = await ngrok.connect(PORT);
-  console.log(url);
-})();
-
 const app = express();
 
-// parse body json with express middleware
-app.use(express.json({ extended: false }));
+// parse url encoded body data with express middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// Create a route that will handle Twilio webhook requests, sent as an
-// HTTP POST to /voice in our application
-app.post(
-  "/voice",
-  // validation webhook. will only require validation when not in test environment
-  // only allows requests coming from twilio
-  twilio.webhook({ validate: process.env.NODE_ENV !== "test" }),
-  (req, res) => {
-    const city = req.body.FromCity;
-
-    // Use the Twilio Node.js SDK to build an XML response
-    const twiml = new VoiceResponse();
-    twiml.say({ voice: "alice" }, `Ohhh, I love ${city}!`);
-    twiml.play({}, "https://demo.twilio.com/docs/classic.mp3");
-
-    // Render the response as XML in reply to the webhook request
-    res.type("text/xml");
-    res.send(twiml.toString());
-  }
-);
+// import routes
+app.use("/voice", require("./routes/voice"));
+app.use("/message", require("./routes/message"));
 
 // Create an HTTP server and listen for requests on env port
 app.listen(PORT, () => {
